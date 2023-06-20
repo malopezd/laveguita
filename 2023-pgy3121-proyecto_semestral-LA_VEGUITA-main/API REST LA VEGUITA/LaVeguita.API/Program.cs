a71d09d0340aada36b937d2553f9ae0875295930
malopezd <ma.lopezd@duocuc.cl>
2023-06-20 00:26:35 +0000

    la veguita

---
+++ 2023-pgy3121-proyecto_semestral-LA_VEGUITA-main/API REST LA VEGUITA/LaVeguita.API/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using LaVeguita.API.Models;
+using LaVeguita.API.Services;
+using LaVeguita.API.Configs;
+using LaVeguita.API.Services.Implementation;
+using LaVeguita.API.Services.Repositorio;
+using LaVeguita.API.Services.Servicios;
+using LaVeguita.API.DbContextos;
+using LaVeguita.API.RepositorioDB;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+namespace LaVeguita.API
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Creando DB...");
+			VentaContext db = new VentaContext();
+			db.Database.EnsureCreated();
+			Console.WriteLine("Ok");
+			Console.ReadKey();
+		}
+	}
+}
+
---
+++ 2023-pgy3121-proyecto_semestral-LA_VEGUITA-main/README.md
@@ -0,0 +1,11 @@
+# LA VEGA ONLINE 
+
+Proyecto Pagina de ventas Online de Verduras, Frutas y Frutos Secos
+
+
+INTEGRANTES:
+* CARLOS CISTERNA
+* CRISTOBAL GUZMAN
+* MATIAS LOPEZ
+* DAVID SALAZAR
+
